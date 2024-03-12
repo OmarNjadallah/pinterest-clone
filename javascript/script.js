bdd660f7df3a252a1abcae6f8809f184355ed078
OmarNjadallah <jadallahomar22@gmail.com>
2024-03-12 15:03:27 +0300

    signup-login-js

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,17 @@ document.getElementById("login").addEventListener('click',function(){
     let list1 = document.getElementById('loginpage');
     list1.classList.replace("hidden","flex");
 });
-document.getElementById("close").addEventListener('click',function(){
+document.getElementById("closelogin").addEventListener('click',function(){
     let list1 = document.getElementById('loginpage');
     list1.classList.replace("flex","hidden");
 });
 
 
+document.getElementById("signup").addEventListener('click',function(){
+    let list1 = document.getElementById('signuppage');
+    list1.classList.replace("hidden","flex");
+});
+document.getElementById("closesignup").addEventListener('click',function(){
+    let list1 = document.getElementById('signuppage');
+    list1.classList.replace("flex","hidden");
+});
