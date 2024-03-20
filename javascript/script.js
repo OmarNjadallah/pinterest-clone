97cf4fb7467c1cad7b996ad67e56d77875ceea15
OmarNjadallah <jadallahomar22@gmail.com>
2024-03-20 14:59:51 +0300

    finishedproject

--- javascript/script.js
+++ javascript/script.js
@@ -27,3 +27,45 @@ document.getElementById("business").addEventListener('click',function(){
     document.getElementById("businessdiv").classList.add('hidden');
     document.getElementById("personaldiv").classList.remove('hidden');
 });
+
+
+document.getElementById("list1").addEventListener('click',function(){
+    clearbuttons();
+    this.classList.replace("bg-gray-300", "bg-yellow-600");
+    let text = document.getElementById('textchanger');
+    text.textContent = 'weekend dinner idea';
+    text.classList.add("text-yellow-600");
+});
+document.getElementById("list2").addEventListener('click',function(){
+    clearbuttons();
+    this.classList.replace("bg-gray-300", "bg-green-900");
+    let text = document.getElementById('textchanger')
+    text.textContent = 'home decor idea'
+    text.classList.replace('text-yellow-600','text-green-900');
+    text.classList.replace('text-blue-600','text-green-900');
+});
+document.getElementById("list3").addEventListener('click',function(){
+    clearbuttons();
+    this.classList.replace("bg-gray-300", "bg-blue-600");
+    let text = document.getElementById('textchanger')
+    text.textContent = 'new look outfit'
+    text.classList.replace('text-green-900','text-blue-600');
+    text.classList.replace('text-yellow-600','text-blue-600');
+
+});
+document.getElementById("list4").addEventListener('click',function(){
+    clearbuttons();
+    this.classList.replace("bg-gray-300", "bg-green-900");
+    let text = document.getElementById('textchanger')
+    text.textContent = 'green thumb idea'
+    text.classList.replace('text-blue-600','text-green-900');
+    text.classList.replace('text-yellow-600','text-green-900');
+});
+
+
+function clearbuttons(){
+    document.getElementById('list1').classList.replace("bg-yellow-600","bg-gray-300")
+    document.getElementById('list2').classList.replace("bg-green-900","bg-gray-300")
+    document.getElementById('list3').classList.replace("bg-blue-600","bg-gray-300")
+    document.getElementById('list4').classList.replace("bg-green-900","bg-gray-300")
+}
